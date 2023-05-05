a3c3e06b8299b629c37e2c826ec3fd836af1520f
ericanthony1286 <nnquangnd@gmail.com>
2023-05-05 10:53:52 +0700

    second

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import { useEffect } from "react";
-import { APP_ID } from "./api/hello";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     window.fbAsyncInit = function () {
       window.FB.init({
-        appId: APP_ID,
+        appId: process.env.APP_ID,
         autoLogAppEvents: true,
         xfbml: true,
         version: "v16.0",
