c3538f8fcd1d1be7d92020dfd393065bd209f852
ericanthony1286 <nnquangnd@gmail.com>
2023-05-05 10:40:36 +0700

    first

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { useEffect } from "react";
+import { APP_ID } from "./api/hello";
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: APP_ID,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v16.0",
+      });
+    };
+  }, []);
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        async
+        defer
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/en_US/sdk.js"
+      />
+    </>
+  );
+}
---
+++ pages/api/hello.ts
@@ -0,0 +1,44 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//require("dotenv").config();
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+const FACEBOOK_GRAPH_URL = "https://graph.facebook.com/v16.0";
+export const APP_ID = process.env.APP_ID;
+const APP_SECRET = process.env.APP_SECRET;
+export default async function handler(req: any, res: any) {
+  const appAccessToken = await getAppAccessToken();
+  const scopes = await debugToken(appAccessToken, req.query.token);
+  console.log(scopes);
+  res.json({ scopes });
+}
+
+const getAppAccessToken = async () => {
+  const response = await fetch(
+    `https://graph.facebook.com/oauth/access_token?client_id=${APP_ID}&client_secret=${APP_SECRET}&grant_type=client_credentials`
+  );
+
+  const data: { access_token: string } = await response.json();
+  if (!response.ok) {
+    throw new Error("App access token failed");
+  }
+  console.log("data.access_token: ", data.access_token);
+  return data.access_token;
+};
+
+const debugToken = async (appAccessToken: string, token: string) => {
+  const response = await fetch(
+    `${FACEBOOK_GRAPH_URL}/debug_token?input_token=${token}&access_token=${appAccessToken}`
+  );
+  console.log("debugtoken: ", response);
+  const data: { data: { scopes: string[] } } = await response.json();
+  return data.data.scopes;
+};
+
+const getPagesBasedOnToken = async (userToken: string) => {
+  const response = await fetch(
+    `${FACEBOOK_GRAPH_URL}/me/accounts?access_token=${userToken}`
+  );
+};
